6d387e9ea23ca25064689cd60b3bcb58f22a7a92
Jackie Wong <924358746@qq.com>
2019-06-06 15:49:24 +0800

    博客首页文件初始化

---
+++ src/pages/index/App.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+    div#app
+        header-top.header-top
+        main.blog-container
+            router-view
+        Footer.footer
+        
+</template>
+
+<script>
+import HeaderTop from '@/pages/index/components/public/Header.vue';
+import Footer from '@/pages/index/components/public/Footer.vue';
+export default {
+    components: {
+        HeaderTop,
+        Footer
+    }
+}
+</script>
+
+<style lang="scss">
+@import '~@/assets/style/common.scss';
+html, body {
+    height: 100%;
+    margin: 0 auto;
+    font-family: 'Titillium Web', sans-serif;
+    background-color: #EBECEE;
+}
+
+div#app {
+    display: flex;
+    flex-direction: column;
+    .header-top {
+        flex: none;
+    }
+    main {
+        flex: 1 0 auto;
+        padding: 40px 0 20px;
+    }
+    .footer {
+        flex: none;
+    }
+}
+</style>
