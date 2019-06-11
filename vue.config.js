34b7ee1b9d52d9268c65a0bb7d3c516292e80f4c
Jackie Wong <924358746@qq.com>
2019-06-11 09:16:05 +0800

    新增cdn资源加载，去除多页面配置

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-    pages: {
-        index: {
-            entry: "src/pages/index/index.js",
-            template: "src/pages/index/index.html",
-            filename: "index.html"
-        }
-    },
     devServer: {
-        historyApiFallback: {
-            rewrites: []
+        historyApiFallback: true
+    },
+    configureWebpack: {
+        externals: {
+            'highlight.js': 'hljs',
+            "moment": "moment",
         }
     }
 };
