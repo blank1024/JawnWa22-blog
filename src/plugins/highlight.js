edba714cc4f9f93d7904931a3fcaecf3ef58bab0
jawnwa22 <924358746@qq.com>
2019-06-12 13:38:20 +0800

    添加注释

--- src/plugins/highlight.js
+++ src/plugins/highlight.js
@@ -1,7 +1,15 @@
-import hljs from 'highlight.js';
+/*
+ * @Description: 导出代码高亮函数
+ * @Date: 2019-06-11 09:23:40
+ * @Author: jawnwa22
+ * @LastEditors: jawnwa22
+ * @LastEditTime: 2019-06-12 13:38:02
+ */
+
+import hljs from "highlight.js";
 
 export default function() {
-    let blocks = document.querySelectorAll('pre code');
+    let blocks = document.querySelectorAll("pre code");
     blocks.forEach(block => {
         hljs.highlightBlock(block);
     });
