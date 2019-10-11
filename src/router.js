10296cb9aba07d2dd10bf3947b7aa7011fa27a92
jawnwa22 <924358746@qq.com>
2019-10-11 22:06:51 +0800

    fix(路由)：修复不存在的about页面引用

--- src/router.js
+++ src/router.js
@@ -17,14 +17,14 @@ export default new Router({
           keepAlive: true
       }
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // },
     {
         path: '/article/:id',
         component: Article,
